28356a298b354a3eb40a8bac55eb33e0ef9718f6
m0ckinjay <m0ckinjay@yandex.com>
2023-10-10 08:35:07 +0300

    with netbeans

---
+++ javaBootCamp/src/main/java/com/m0ckinjay/javabootcamp/JavaBootCamp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.m0ckinjay.javabootcamp;
+
+/**
+ *
+ * @author mo
+ */
+public class JavaBootCamp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
