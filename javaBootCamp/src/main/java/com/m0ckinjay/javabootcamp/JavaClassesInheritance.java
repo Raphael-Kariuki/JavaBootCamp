298d560612817c3b1b8ea0a9a65c806e0991147d
m0ckinjay <m0ckinjay@yandex.com>
2023-10-28 09:16:16 +0300

    enum usage in inheritance

--- javaBootCamp/src/main/java/com/m0ckinjay/javabootcamp/JavaClassesInheritance.java
+++ javaBootCamp/src/main/java/com/m0ckinjay/javabootcamp/JavaClassesInheritance.java
@@ -4,27 +4,68 @@
  */
 package com.m0ckinjay.javabootcamp;
 
+
 /**
  *
  * @author mo
  */
 
+enum Gender{
+    MALE(1,'M'), FEMALE(2, 'F');
+    
+    private final Integer G_INDEX;
+
+    private final Character G_ABRV;
+    
+    Gender(Integer G_INDEX, Character G_ABRV){
+        this.G_INDEX =G_INDEX;
+        this.G_ABRV =G_ABRV;
+        
+    }
+  
+    public Integer getG_INDEX() {
+        return G_INDEX;
+    }
+
+    public Character getG_ABRV() {
+        return G_ABRV;
+    }
+    
+ 
+    
+}
+
 abstract class Human{
 
+    
+    private String name;
+    private Integer age;
+    private Integer height;
+    public static final int LIFESPAN = 100;
+    private Gender gender;
+    
+    
     public Human(String name, Integer age) {
         this.name = name;
         this.age = age;
     }
 
     public Human(String name, Integer age, Integer height) {
-        this.name = name;
-        this.age = age;
+        this(name, age);
         this.height = height;
     }
-    private String name;
-    private Integer age;
-    private Integer height;
-    public static final int LIFESPAN = 100;
+  
+
+
+    
+    /**
+    * @return the gender
+    */
+    public Gender getGender() {
+        return gender;
+    }
+
+
 
     /**
      * @return the name
@@ -93,6 +134,8 @@ class Musician extends Human{
         
     }
 
+   
+
     @Override
     void printTTL(){
         System.out.println("Lifespan " + getLIFESPAN());
@@ -105,14 +148,36 @@ class Teacher extends Human{
     String institution;
     String Grade;
     private final static int LIFESPAN = 99;
+
+    public void setInstitution(String institution) {
+        this.institution = institution;
+    }
+
+    public void setGrade(String Grade) {
+        this.Grade = Grade;
+    }
+
+    public String getInstitution() {
+        return institution;
+    }
+
+    public String getGrade() {
+        return Grade;
+    }
+
+    public static int getLIFESPAN() {
+        return LIFESPAN;
+    }
+   
     
     public Teacher(String name, Integer age, Integer height) {
         super(name, age, height);
     }
     @Override
     void printTTL(){
-        System.out.println("I have " + (super.LIFESPAN - getAge()) + " to teach");
+        System.out.println("I have " + (Human.LIFESPAN - getAge()) + " to teach");
         System.out.println("I have " + (LIFESPAN - getAge()) + " to teach");
+        System.out.println("Gender " + Gender.MALE.getG_ABRV());
     }
     
     }
@@ -120,6 +185,7 @@ public class JavaClassesInheritance {
     public static void main(String[] args) {
         Musician musician = new Musician("Mojay", 26, 167);
         musician.printTTL();
+        
         Teacher teacher = new Teacher("Mojay", 25, 168);
         teacher.printTTL();
     }
