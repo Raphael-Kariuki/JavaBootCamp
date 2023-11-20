ea54d753aac273719313de35f3567da528e5b262
m0ckinjay <m0ckinjay@yandex.com>
2023-11-20 11:50:02 +0300

    deal 10 cards

--- javaBootCamp/src/main/java/com/m0ckinjay/javabootcamp/cardShuffling/Main.java
+++ javaBootCamp/src/main/java/com/m0ckinjay/javabootcamp/cardShuffling/Main.java
@@ -11,12 +11,15 @@ import java.security.SecureRandom;
  * @author mo
  */
 public class Main {
+    
+    public static SecureRandom secureRandom = new SecureRandom();
+    protected static  int NUMBER_OF_CARDS = 52;
+
     static void shuffle(DeckOfCards deckOfCards){
         
         //get array of cards
         var cardsToShuffle = deckOfCards.deckOfCards;
         
-        SecureRandom secureRandom = new SecureRandom();
         
         for (int i = 0; i < cardsToShuffle.length - 1; i++) {
             
@@ -30,15 +33,39 @@ public class Main {
         }
     
     }
+    static Card dealCard(DeckOfCards deckOfCards){
+        //decrement the number of cards everytime method is called
+        //to simulate decrease of cards on deck on dealing
+        --NUMBER_OF_CARDS;
+        
+        //initialize to allow return
+        Card card = null;
+        
+        //ensure that deck of cards still has cards before dealing
+        if (NUMBER_OF_CARDS > 0) {
+        //return a random card
+            card = deckOfCards.deckOfCards[secureRandom.nextInt(51)];
+        }else{
+            System.out.println("Out of cards");
+        }
+        return card;
+        
+    }
     public static void main(String[] args) {
         
         DeckOfCards deckOfCards = new DeckOfCards();
+        
+        //shuffle the deck
         shuffle(deckOfCards);
+        
+        
+        //deal 10 cards
+        for (int i = 0; i < 10; i++) {
+            System.out.println(""+ dealCard(deckOfCards).getCard());
 
-        for (Card card : deckOfCards.deckOfCards) {
-            System.out.println("" + card.getCard());
         }
         
+
        
         
        
