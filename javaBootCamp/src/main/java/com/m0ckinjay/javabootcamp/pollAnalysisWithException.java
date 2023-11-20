767c3f37b4ebd597b38f01c98a39835eb885727b
m0ckinjay <m0ckinjay@yandex.com>
2023-11-20 09:40:06 +0300

    randomize values with secureRandom and print analysis: sepatared in methods

--- javaBootCamp/src/main/java/com/m0ckinjay/javabootcamp/pollAnalysisWithException.java
+++ javaBootCamp/src/main/java/com/m0ckinjay/javabootcamp/pollAnalysisWithException.java
@@ -11,14 +11,13 @@ import java.security.SecureRandom;
  * @author mo
  */
 public class pollAnalysisWithException {
-    public static void main(String[] args) {
-        
+    static int [] generatepollArray(){
+                
         //to generate random values between 0 and 10
         SecureRandom secureRandom = new SecureRandom();
         
         //declare arrays to use
         int[] pollResults = new int[12];
-        int[] frequency = new int[10];
         
         //add values to frequency array less the last index
         for (int i = 0; i < pollResults.length - 2; i++) {
@@ -26,28 +25,41 @@ public class pollAnalysisWithException {
         }
         
         //add value that should throw an exception
-        pollResults[pollResults.length - 1] = 14;
+        pollResults[pollResults.length - 1] = secureRandom.nextInt(10,20);
         
+        return pollResults;
+    
+    }
+    
+    static int[] returnPollAnalysisArray(int [] x){
+         //declare arrays to use
+        int[] frequency = new int[10];
 
         //increment frequency array based on pollresult values catching error
         try {
-            for (int i = 0; i < pollResults.length; i++) {
-                ++frequency[pollResults[i]];
+            for (int i = 0; i < x.length; i++) {
+                ++frequency[x[i]];
         }    
         } catch (Exception e) {
             System.out.println("" + e);
         }
-        for (int i = 0; i < frequency.length; i++) {
+        return frequency;
+    } 
+    public static void main(String[] args) {
+
+        
+       int[] pollResults = returnPollAnalysisArray(generatepollArray());
+        for (int i = 0; i < pollResults.length; i++) {
             System.out.printf("%3d" , i);
         }
                         System.out.println("");
 
-        for (int i : frequency) {
+        for (int i : pollResults) {
             System.out.printf("%3d" , i);
         }
                 System.out.println("");
 
-        for (int pollResult : pollResults) {
+        for (int pollResult : generatepollArray()) {
             System.out.printf("%3d" , pollResult);
         }
         
