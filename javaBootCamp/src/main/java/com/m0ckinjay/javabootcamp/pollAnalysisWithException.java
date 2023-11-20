0a4549d99a8ad0c5a96eb5b53012b9dc1168e552
m0ckinjay <m0ckinjay@yandex.com>
2023-11-20 09:42:15 +0300

    randomize values with secureRandom and print analysis: sepatared in methods : dynamic size

--- javaBootCamp/src/main/java/com/m0ckinjay/javabootcamp/pollAnalysisWithException.java
+++ javaBootCamp/src/main/java/com/m0ckinjay/javabootcamp/pollAnalysisWithException.java
@@ -11,13 +11,13 @@ import java.security.SecureRandom;
  * @author mo
  */
 public class pollAnalysisWithException {
-    static int [] generatepollArray(){
+    static int [] generatepollArray(int size){
                 
         //to generate random values between 0 and 10
         SecureRandom secureRandom = new SecureRandom();
         
         //declare arrays to use
-        int[] pollResults = new int[12];
+        int[] pollResults = new int[size];
         
         //add values to frequency array less the last index
         for (int i = 0; i < pollResults.length - 2; i++) {
@@ -48,7 +48,7 @@ public class pollAnalysisWithException {
     public static void main(String[] args) {
 
         
-       int[] pollResults = returnPollAnalysisArray(generatepollArray());
+       int[] pollResults = returnPollAnalysisArray(generatepollArray(20));
         for (int i = 0; i < pollResults.length; i++) {
             System.out.printf("%3d" , i);
         }
@@ -59,7 +59,7 @@ public class pollAnalysisWithException {
         }
                 System.out.println("");
 
-        for (int pollResult : generatepollArray()) {
+        for (int pollResult : generatepollArray(20)) {
             System.out.printf("%3d" , pollResult);
         }
         
