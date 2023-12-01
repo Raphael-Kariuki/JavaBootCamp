ba642c40df86976744386c9cb00660cac7156d27
m0ckinjay <m0ckinjay@yandex.com>
2023-12-01 07:06:33 +0300

    author and book root resources

---
+++ rest-server/src/main/java/com/m0ckinjay/rest/server/rest/BookResource.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.m0ckinjay.rest.server.rest;
+
+import jakarta.ejb.Stateless;
+import jakarta.ws.rs.Path;
+
+/**
+ *
+ * @author mo
+ */
+@Stateless
+@Path("/books")
+public class BookResource {
+    
+}
