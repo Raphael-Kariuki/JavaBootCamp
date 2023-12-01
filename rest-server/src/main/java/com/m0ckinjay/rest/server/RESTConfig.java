fc7e683c17f74e42ae9691a2b39f12f733684af1
m0ckinjay <m0ckinjay@yandex.com>
2023-12-01 07:05:50 +0300

    Application subclass

---
+++ rest-server/src/main/java/com/m0ckinjay/rest/server/RESTConfig.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.m0ckinjay.rest.server;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ *
+ * @author mo
+ */
+@ApplicationPath("/api")
+public class RESTConfig extends Application{
+    
+}
