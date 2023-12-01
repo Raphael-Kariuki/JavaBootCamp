373cd16c6060cbd072acad6cc1d509a828ef423e
m0ckinjay <m0ckinjay@yandex.com>
2023-12-01 19:58:26 +0300

    Author entity

---
+++ bookstore/src/main/java/com/m0ckinjay/domain/Author.java
@@ -0,0 +1,96 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.m0ckinjay.domain;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author mo
+ */
+public class Author {
+        
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String blogURL;
+
+    public Author(){}
+    public Author(String id, String firstName, String lastName, String blogURL) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.blogURL = blogURL;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 67 * hash + Objects.hashCode(this.id);
+        hash = 67 * hash + Objects.hashCode(this.firstName);
+        hash = 67 * hash + Objects.hashCode(this.lastName);
+        hash = 67 * hash + Objects.hashCode(this.blogURL);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Author other = (Author) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        if (!Objects.equals(this.firstName, other.firstName)) {
+            return false;
+        }
+        if (!Objects.equals(this.lastName, other.lastName)) {
+            return false;
+        }
+        return Objects.equals(this.blogURL, other.blogURL);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getBlogURL() {
+        return blogURL;
+    }
+
+    public void setBlogURL(String blogURL) {
+        this.blogURL = blogURL;
+    }
+    
+    
+    
+}
