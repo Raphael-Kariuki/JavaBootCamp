c6443be68b88c24ef826e0c509bfaa1d5b2cb77f
m0ckinjay <m0ckinjay@yandex.com>
2023-12-01 20:07:36 +0300

    Author web resource

---
+++ bookstore/src/main/java/com/m0ckinjay/infrastructure/restResources/AuthorResource.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.m0ckinjay.infrastructure.restResources;
+
+import jakarta.ejb.Stateless;
+import jakarta.ws.rs.Path;
+
+/**
+ *
+ * @author mo
+ */
+@Stateless
+@Path("/authors")
+public class AuthorResource {
+    
+}
