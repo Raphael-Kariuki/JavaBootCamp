06ae5d0cb681d565a3ee5195048df98725653fdc
m0ckinjay <m0ckinjay@yandex.com>
2023-12-04 06:43:28 +0300

    rest resource perfoming insert to postgresql

---
+++ crud/src/main/java/com/m0ckinjay/crud/MyResource.java
@@ -0,0 +1,36 @@
+package com.m0ckinjay.crud;
+
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+/**
+ * Root resource (exposed at "myresource" path)
+ */
+@Path("person")
+public class MyResource {
+    PersonService personService = new PersonService();
+    /**
+     * Method handling HTTP GET requests. The returned object will be sent
+     * to the client as "text/plain" media type.
+     *
+     * @return String that will be returned as a text/plain response.
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getIt() {
+        return "Got it!";
+    }
+    
+    @Path("new")
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response createPersonEntry(PersonModel person){
+        return Response.ok(personService.insertPerson(person)).build();
+    }
+}
