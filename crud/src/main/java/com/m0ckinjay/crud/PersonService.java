1d3d1f0cac260a6299bd19d22c25d1c6a3f7ea33
m0ckinjay <m0ckinjay@yandex.com>
2023-12-05 09:43:42 +0300

    sort getPersonById to return person object not list of person objects

--- crud/src/main/java/com/m0ckinjay/crud/PersonService.java
+++ crud/src/main/java/com/m0ckinjay/crud/PersonService.java
@@ -76,32 +76,32 @@ public class PersonService {
         }
        return dataFromDb;
     }
-    public List<PersonModel> getPersonById(int id) throws SQLException{
-        List<PersonModel> dataFromDb = new ArrayList<>();
+    public PersonModel getPersonById(int id) throws SQLException{
         
         String selectQuery = "select * from person where entryid = ?";
         
         PreparedStatement selectPreparedStatement = null;
         ResultSet rs = null;
+        PersonModel person = new PersonModel();
+
         
         try {
             selectPreparedStatement = conn.prepareStatement(selectQuery);
             selectPreparedStatement.setInt(1, id);
 
             rs = selectPreparedStatement.executeQuery();
+
             while (rs.next()) {
-                PersonModel person = new PersonModel();
                 person.setFirstname(rs.getString("firstname"));
                 person.setLastname(rs.getString("lastname"));
                 person.setAge(rs.getInt("age"));
                 person.setHeight(rs.getDouble("height"));
-                dataFromDb.add(person);
             }
 
         } catch (SQLException e) {
             System.out.println("" + e.getMessage());
         }
-       return dataFromDb;
+       return person;
     }
     public int deletePersonById(int id){
         String deleteQuery = "delete from person where entryid = ?";
