f0ce0d920e03ea9cc2e3189cb773ebcb5e16a53c
m0ckinjay <m0ckinjay@yandex.com>
2023-12-06 06:58:50 +0300

    temporary redirect back to home page on successful update

--- crud/src/main/java/com/m0ckinjay/crud/MyResource.java
+++ crud/src/main/java/com/m0ckinjay/crud/MyResource.java
@@ -10,8 +10,14 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.MultivaluedMap;
 import jakarta.ws.rs.core.Response;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 /**
  * Root resource (exposed at "myresource" path)
@@ -61,11 +67,53 @@ public class MyResource {
     }
     
     @Path("{id}/update")
-    @PUT
-    @Consumes(MediaType.APPLICATION_JSON)
+    @POST
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response updatePersonById(@PathParam("id") int id, PersonModel newPersonModel){
-        return Response.ok(personService.updatePersonById(newPersonModel, id)).build();
+    public Response updatePersonById(@PathParam("id") int id, MultivaluedMap<String, String> formDataMap) throws URISyntaxException{
+          /*
+        By default a multiValued hashMap values are list objects containing the correspondind form data items/values.
+        When fed to a response directly , the json is not nicely formated
+        (key, value(key, value)) insted of (key, value) - soln.Iterate and put in hashMap
+        */
+        //hashMap declaration
+        Map<String,String> formParameters = new HashMap<>();
+
+        //iterate the keys
+        Iterator<String> it = formDataMap.keySet().iterator();
+
+
+        while(it.hasNext()){
+           String theKey = (String)it.next();
+           formParameters.put(theKey,formDataMap.getFirst(theKey));
+        }
+        PersonModel updatePersonModel = new PersonModel();
+        updatePersonModel.setFirstname(formParameters.get("firstName"));
+        updatePersonModel.setLastname(formParameters.get("lastName"));
+        updatePersonModel.setAge(Integer.parseInt(formParameters.get("age")));
+        updatePersonModel.setHeight(Double.parseDouble(formParameters.get("height")));
+        int responseInt = personService.specialUpdatePersonById(updatePersonModel, id);
+        Response response = null;
+        if (responseInt == 1) {
+            response = Response.temporaryRedirect(URI.create("/")).build();
+        }else{
+        response = Response.status(Response.Status.NOT_IMPLEMENTED).location(new URI("/")).build();
+        }
+        return response;
+    }
+//    @Path("{id}/update")
+//    @PUT
+//    @Consumes(MediaType.APPLICATION_JSON)
+//    @Produces(MediaType.APPLICATION_JSON)
+//    public Response updatePersonById(@PathParam("id") int id, PersonModel newPersonModel){
+//        return Response.ok(personService.updatePersonById(newPersonModel, id)).build();
+//    }
+    @Path("{id}/update_")
+    @POST
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response specialUpdatePersonById(@PathParam("id") int id, PersonModel newPersonModel){
+        return Response.ok(personService.specialUpdatePersonById(newPersonModel, id)).build();
     }
     @Path("{id}/patch")
     @PATCH
