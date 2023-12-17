08abccfbacb29ed157c05fc853f86cc7684d24fb
m0ckinjay <m0ckinjay@yandex.com>
2023-12-17 21:15:27 +0300

    entity class from db

---
+++ restServer/src/main/java/com/m0ckinjay/restserver/db/users.java
@@ -0,0 +1,153 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.m0ckinjay.restserver.db;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author mo
+ */
+//@Entity
+//@Table(name = "systemusers")
+//@NamedQueries({
+//    @NamedQuery(name = "users.findAll", query = "SELECT u FROM users u")})
+public class users implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Basic(optional = false)
+    @Column(name = "entryid")
+    private int entryid;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "username")
+    private String username;
+    @Basic(optional = false)
+    @Column(name = "firstname")
+    private String firstname;
+    @Basic(optional = false)
+    @Column(name = "lastname")
+    private String lastname;
+    @Basic(optional = false)
+    @Column(name = "emailaddress")
+    private String emailaddress;
+    @Basic(optional = false)
+    @Column(name = "password")
+    private String password;
+    @Basic(optional = false)
+    @Column(name = "log_ts")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date logTs;
+
+    public users() {
+    }
+
+    public users(String username) {
+        this.username = username;
+    }
+
+    public users(String username, int entryid, String firstname, String lastname,
+            String emailaddress, String password, Date logTs) {
+        this.username = username;
+        this.entryid = entryid;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.emailaddress = emailaddress;
+        this.password = password;
+        this.logTs = logTs;
+    }
+
+    public int getEntryid() {
+        return entryid;
+    }
+
+    public void setEntryid(int entryid) {
+        this.entryid = entryid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmailaddress() {
+        return emailaddress;
+    }
+
+    public void setEmailaddress(String emailaddress) {
+        this.emailaddress = emailaddress;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getLogTs() {
+        return logTs;
+    }
+
+    public void setLogTs(Date logTs) {
+        this.logTs = logTs;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (username != null ? username.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof users)) {
+            return false;
+        }
+        users other = (users) object;
+        if ((this.username == null && other.username != null) || (this.username != null && !this.username.equals(other.username))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.m0ckinjay.restserver.db.users[ username=" + username + " ]";
+    }
+    
+}
